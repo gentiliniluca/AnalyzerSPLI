217574f1095603c5d979850f40af7863a6f0c2f9
Davide Ghinato <dghinato@gmail.com>
2015-03-20 15:16:03 +0100

    Versione buona B_B

--- Versione2/analyzer/liv7.c
+++ Versione2/analyzer/liv7.c
@@ -5,6 +5,8 @@ void liv7(u_int len,const u_char *p, u_int ssap, u_int dsap) {
 	int i;
 	int flag_ftp_req = 0;
 	int flag_ftp_res = 0;
+	int flag_ftp_dat = 0;
+	int flag_ftp_raw = 0;
 
   	if((int)len<=0) {
   		return;
@@ -17,39 +19,49 @@ void liv7(u_int len,const u_char *p, u_int ssap, u_int dsap) {
 			return;
 		}
 		
-    	if(p_ftp) {	
-    		if (ssap == 21) {
-    			myprintf("FTP Response|\n");
-    			flag_ftp_res = 1;
-    		}
-    		if(dsap == 21){
-    			myprintf("FTP Request|\n");
-    			flag_ftp_req = 1;
-    		}
-    		if (ssap == 20) {
-    			myprintf("FTP-DATA Response|\n");
-    			flag_ftp_res = 1;
-    		}
-    		if(dsap == 20){
-    			myprintf("FTP-DATA Request|\n");
-    			flag_ftp_req = 1;
-    		}
-    	
+	    	if(p_ftp) {	
+	    		if (ssap == 21) {
+	    			myprintf("FTP | Response\n    | ");
+	    			flag_ftp_res = 1;
+	    		}
+	    		if(dsap == 21){
+	    			myprintf("FTP | Request\n    | ");
+	    			flag_ftp_req = 1;
+	    		}
+	    		if (ssap == 20) {
+	    			myprintf("FTP | Data\n\n");
+	    			flag_ftp_dat = 1;
+	    		}
+	    		if(dsap == 20){
+	    			myprintf("FTP | Data\n\n");
+	    			flag_ftp_dat = 1;
+	    		}
+    		
 		  	for(i=1; i<=len; i++) {
 				if(isprint(*p)) {
 					myprintf("%c",*p);
 				} else {
-					myprintf(".");
+					if(((*p) == '\n') || ((*p) == '\r') || ((*p) == '\t')) {
+						myprintf("%c",*p);
+					} else	{
+						myprintf(".");
+						flag_ftp_raw = 1;
+					}
 				}
+			
 				if(isascii(*p)){
 					fprintf(mem,"%c",*p);
 				} else {
 					fprintf(mem,".");
 				}
-				p++;
 				/*if((i%70)==0){
 					myprintf("\n     |");
 				}*/
+				
+				p++;
+		  	}
+		  	if(flag_ftp_raw) {
+		  		myprintf("\n");
 		  	}
 		  	myprintf("\n");
 		  	fflush(mem);
